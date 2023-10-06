158b55d5223856faa462de76ee809fa9c371a786
YejeeCho <choyejee14@gmail.com>
2023-10-06 13:56:50 +0900

    Fix: 공통 Input 컴포넌트 높이값 조정 및 label 추가

--- Components/Input.tsx
+++ Components/Input.tsx
@@ -4,15 +4,15 @@ import { UseFormRegisterReturn } from "react-hook-form";
 
 interface IInput extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
-  type?: "text" | "number" | "date";
+  type?: "text" | "number" | "date" | "file";
   register?: UseFormRegisterReturn;
 }
 
 export const Input = ({ label, type = "text", register, ...attr }: IInput) => {
   return (
     <InputContainer>
-      <Label>{label}</Label>
-      <InputBox type={type} {...register} {...attr} />
+      <Label htmlFor="input">{label}</Label>
+      <InputBox type={type} {...register} {...attr} id="input" />
     </InputContainer>
   );
 };
@@ -28,5 +28,5 @@ const Label = styled.label`
 `;
 
 const InputBox = styled.input`
-  height: 20px;
+  height: 22px;
 `;
